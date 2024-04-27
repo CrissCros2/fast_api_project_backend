8d205fbd0c1ffe9999411b31c295926b733d5b36
Chris <christopher.roberts-10@student.manchester.ac.uk>
2024-04-27 12:20:32 +0100

    Add root redirect to docs for now

--- v0/main.py
+++ v0/main.py
@@ -1,4 +1,13 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+from fastapi import status
 
 app = FastAPI()
 
+
+@app.get("/", status_code=status.HTTP_301_MOVED_PERMANENTLY)
+async def root():
+    """
+    Redirects the user to the docs page
+    """
+    return RedirectResponse("/docs")
