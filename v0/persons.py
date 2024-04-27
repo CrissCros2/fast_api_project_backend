53727a0160409abfea7b7129518e5942f5f76810
Chris <christopher.roberts-10@student.manchester.ac.uk>
2024-04-27 19:40:35 +0100

    Add get all persons

--- v0/persons.py
+++ v0/persons.py
@@ -1,4 +1,15 @@
 from fastapi import APIRouter
+from uuid import uuid4
+
+from api_models import Person
 
 
 persons = APIRouter()
+
+
+@persons.get("/")
+def get_persons() -> list[Person]:
+    """
+    Get all persons
+    """
+    return [Person(id=uuid4(), name=str(uuid4()))]
