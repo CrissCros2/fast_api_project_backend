5670d38f45307bab7492ce83d6fd6b95f0ed6f2f
Chris <christopher.roberts-10@student.manchester.ac.uk>
2024-04-27 19:42:14 +0100

    Add create person method

--- v0/persons.py
+++ v0/persons.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 from uuid import uuid4
 
+from starlette import status
+
 from api_models import Person
 
 
@@ -13,3 +15,11 @@ def get_persons() -> list[Person]:
     Get all persons
     """
     return [Person(id=uuid4(), name=str(uuid4()))]
+
+
+@persons.post("/", status_code=status.HTTP_201_CREATED)
+def create_person(person: Person) -> Person:
+    """
+    Create a new person
+    """
+    return person
